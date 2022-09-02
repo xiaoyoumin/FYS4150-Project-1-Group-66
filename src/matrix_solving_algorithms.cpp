aa098cad16152f710a08d4d9bfcc9e2442e5c3a8
david <david.svejda2000@gmail.com>
2022-09-02 12:03:50 +0200

    Added src and headder folders, and made a start on the general algorithm

---
+++ src/matrix_solving_algorithms.cpp
@@ -0,0 +1,16 @@
+//
+// Functions for calculating the solutions of matrixequations with tridiagonal matrices
+//
+
+#include "matrix_solving_algorithms.hpp"
+
+
+// Solve a general matrix equation
+std::vector<double> general_algorithm(
+    std::vector<double> a,
+    std::vector<double> b,
+    std::vector<double> c,
+    std::vector<double> g)
+    {
+        std::vector<double> v;
+    }
