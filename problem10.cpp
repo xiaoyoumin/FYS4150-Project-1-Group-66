e4cbe741968d729d81995b1ff36876c760226fb1
Chaoran Han <54284515+xiaoyoumin@users.noreply.github.com>
2022-09-14 07:05:32 +1200

    Add files via upload

---
+++ problem10.cpp
@@ -0,0 +1,108 @@
+#include<iostream>
+#include<fstream>
+#include<cmath>
+#include<vector>
+#include <time.h>
+
+using namespace std;
+
+//general algorithm
+int Thom_algr(int n, vector<double> a, vector<double> b, vector<double> c, vector<double> g, vector<double> &v)
+{
+    vector<double> b_tilde(n), g_tilde(n);
+    int i;
+
+    //check illegal input
+    if(a.size()!=n-1 || c.size()!=n-1 || b.size()!=n || g.size()!=n || v.size()!=n) return -1;
+
+    b_tilde[0] = b[0];
+    for(i = 1; i<n; i++)
+        b_tilde[i] = b[i]-(a[i-1]*c[i-1])/b_tilde[i-1];
+
+    g_tilde[0] = g[0];
+    for(i=1; i<n; i++)
+        g_tilde[i] = g[i]-(a[i-1]*g_tilde[i-1])/b_tilde[i-1];
+
+    v[n-1] = g_tilde[n-1]/b_tilde[n-1];
+    for(i=n-2; i>=0; i--)
+        v[i] = (g_tilde[i]-c[i]*v[i+1])/b_tilde[i];
+
+    return 0;
+}
+
+
+int simp_Thom_algr(int n, vector<double> g, vector<double> &v)
+{
+    vector<double> b_tilde(n), g_tilde(n);
+    int i;
+
+    b_tilde[0] = 2;
+    for(i = 1; i<n; i++)
+        b_tilde[i] = 2-((-1)*(-1))/b_tilde[i-1];
+
+    g_tilde[0] = g[0];
+    for(i=1; i<n; i++)
+        g_tilde[i] = g[i]-((-1)*g_tilde[i-1])/b_tilde[i-1];
+
+    v[n-1] = g_tilde[n-1]/b_tilde[n-1];
+    for(i=n-2; i>=0; i--)
+        v[i] = (g_tilde[i]-(-1)*v[i+1])/b_tilde[i];
+
+    return 0;
+}
+
+
+
+
+int main ()   //code obtained from the code samples of the in4150 coursepage
+{
+
+
+    int n_step = 1000000;
+    int n = n_step-1;
+    int i;
+    double h = 1.0/n_step;
+    vector<double> a(n-1, -1), b(n, 2), c(n-1, -1), v(n), g(n), x(n_step+1);
+
+    for(i=0; i<n; i++)
+        g[i] = 100*exp(-10*(i+1)*h)*(h*h);
+
+    
+
+
+  // Start measuring runtime for special algorithm 10 times
+  for(i=0; i<16; i++)
+  {
+    clock_t t1 = clock();
+  
+    // The code you want to perform timing on
+    simp_Thom_algr(n, g, v);
+
+    // Stop measuring time
+    clock_t t2 = clock();
+    // Calculate the elapsed time.
+    double duration_seconds = ((double) (t2 - t1)) / CLOCKS_PER_SEC;
+    cout << duration_seconds <<" ";
+  }
+
+cout <<"\n";
+
+  // Start measuring runtime for general algorithm 10 times
+  for(i=0; i<16; i++)
+  {
+    clock_t t1 = clock();
+
+    // The code you want to perform timing on
+    Thom_algr(n, a, b, c, g, v);
+  
+    // Stop measuring time
+    clock_t t2 = clock();
+    // Calculate the elapsed time.
+    double duration_seconds = ((double) (t2 - t1)) / CLOCKS_PER_SEC;
+    cout << duration_seconds <<" ";
+  }
+
+  
+
+  return 0;
+}
